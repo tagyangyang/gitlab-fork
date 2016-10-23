c452a3738422884ce22f4b64113d3dc89224a541
David Wagner <david@marvid.fr>
2016-10-23 14:17:11 +0200

    Drop legacy and unused ci_projects table
    This table isn't used anymore since e80e3f5372d6bcad1fbe04a85b3086bb66794828.
    
    Signed-off-by: David Wagner <david@marvid.fr>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118183841) do
+ActiveRecord::Schema.define(version: 20161121143317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -273,29 +273,6 @@ ActiveRecord::Schema.define(version: 20161118183841) do
     t.string "job_type", default: "parallel"
     t.string "refs"
     t.datetime "deleted_at"
-  end
-
-  create_table "ci_projects", force: :cascade do |t|
-    t.string "name"
-    t.integer "timeout", default: 3600, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "token"
-    t.string "default_ref"
-    t.string "path"
-    t.boolean "always_build", default: false, null: false
-    t.integer "polling_interval"
-    t.boolean "public", default: false, null: false
-    t.string "ssh_url_to_repo"
-    t.integer "gitlab_id"
-    t.boolean "allow_git_fetch", default: true, null: false
-    t.string "email_recipients", default: "", null: false
-    t.boolean "email_add_pusher", default: true, null: false
-    t.boolean "email_only_broken_builds", default: true, null: false
-    t.string "skip_refs"
-    t.string "coverage_regex"
-    t.boolean "shared_runners_enabled", default: false
-    t.text "generated_yaml_config"
   end
 
   create_table "ci_runner_projects", force: :cascade do |t|
