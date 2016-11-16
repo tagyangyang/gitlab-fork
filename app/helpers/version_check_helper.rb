efefc7eae8e37abcdab8786de1eb84801bd37a2d
Matt Lee <mattl@gitlab.com>
2016-11-16 17:25:14 -0500

    Adds a link to the CHANGELOG.md file to the up-to-date button

--- app/helpers/version_check_helper.rb
+++ app/helpers/version_check_helper.rb
@@ -1,7 +1,7 @@
 module VersionCheckHelper
   def version_status_badge
     if Rails.env.production? && current_application_settings.version_check_enabled
-      image_tag VersionCheck.new.url
+      link_to image_tag(VersionCheck.new.url), "https://gitlab.com/gitlab-org/gitlab-ce/blob/master/CHANGELOG.md", :target => "_blank"
     end
   end
 end
