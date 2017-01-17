adfbba05a378d8ba87c497573247efb72c897f55
Nur Rony <pro.nmrony@gmail.com>
2017-01-17 17:05:15 +0600

    resolves eslint issues

--- features/support/phantomjs/disable_animations.js
+++ features/support/phantomjs/disable_animations.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names, space-before-function-paren, no-var, space-before-blocks, prefer-rest-params, wrap-iife, camelcase, no-param-reassign, quotes, prefer-template, no-new, comma-dangle, one-var, one-var-declaration-per-line, prefer-arrow-callback, no-else-return, no-unused-vars, padded-blocks, max-len */
 var disableAnimationStyles = '-o-transition: none !important; ' +
   '-moz-transition: none !important; ' +
   '-ms-transition: none !important; ' +
@@ -14,7 +15,7 @@ var disableAnimationStyles = '-o-transition: none !important; ' +
   '-ms-animation: none !important;' +
   'animation: none !important;';
 
-window.onload = function() {
+window.onload = function disableCSSAnimationForTest() {
   var animationStyles = document.createElement('style');
   animationStyles.type = 'text/css';
   animationStyles.innerHTML = '* {' + disableAnimationStyles + '}';
