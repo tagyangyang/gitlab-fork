862124bb1eb9f5ae63fe32aace63bfebe4e1fa8f
Lin Jen-Shin <godfat@godfat.org>
2017-02-24 00:56:38 +0800

    Update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216141440) do
+ActiveRecord::Schema.define(version: 20170222143603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
