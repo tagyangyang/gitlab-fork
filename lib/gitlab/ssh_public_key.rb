896921b8401fa3186f9a072d42dce6fc34025276
Rémy Coutable <remy@rymai.me>
2017-02-24 19:05:17 +0100

    Remove an unused method
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- lib/gitlab/ssh_public_key.rb
+++ lib/gitlab/ssh_public_key.rb
@@ -84,22 +84,5 @@ module Gitlab
         nil
       end
     end
-
-    def explicit_fingerprint_algorithm?
-      # OpenSSH 6.8 introduces a new default output format for fingerprints.
-      # Check the version and decide which command to use.
-
-      version_output, version_status = popen(%w[ssh -V])
-      return false unless version_status.zero?
-
-      version_matches = version_output.match(/OpenSSH_(?<major>\d+)\.(?<minor>\d+)/)
-      return false unless version_matches
-
-      version_info = Gitlab::VersionInfo.new(version_matches[:major].to_i, version_matches[:minor].to_i)
-
-      required_version_info = Gitlab::VersionInfo.new(6, 8)
-
-      version_info >= required_version_info
-    end
   end
 end
