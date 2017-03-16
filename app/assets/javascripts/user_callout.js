b62ecfa33c0cd266ef47394f350b4d6bc88a25bb
tauriedavis <taurie@gitlab.com>
2017-03-16 15:46:56 -0700

    29595 Customize experience callout design

--- app/assets/javascripts/user_callout.js
+++ app/assets/javascripts/user_callout.js
@@ -2,6 +2,24 @@ import Cookies from 'js-cookie';
 
 const USER_CALLOUT_COOKIE = 'user_callout_dismissed';
 
+const USER_CALLOUT_TEMPLATE = `
+  <div class="bordered-box landing content-block">
+    <button class="btn btn-default close close-user-callout" type="button" aria-label="Close">
+      <i class="fa fa-times dismiss-icon"></i>
+    </button>
+    <div class="svg-container"></div>
+    <div class="user-callout-copy">
+      <h4>
+        Customize your experience
+      </h4>
+      <p>
+        Change syntax themes, default project pages, and more in preferences.
+      </p>
+      <a class="btn btn-primary user-callout-btn" href="/profile/preferences">Check it out</a>
+    </div>
+  </div>
+</div>`;
+
 export default class UserCallout {
   constructor() {
     this.isCalloutDismissed = Cookies.get(USER_CALLOUT_COOKIE);
