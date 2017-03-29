7586fee9ed4afaac9876a59057e49c5ec0154070
Alfredo Sumaran <a.sumaran@gmail.com>
2017-03-29 02:11:37 -0500

    Remove IIFEs

--- app/assets/javascripts/breakpoints.js
+++ app/assets/javascripts/breakpoints.js
@@ -1,65 +1,52 @@
 /* eslint-disable func-names, space-before-function-paren, wrap-iife, one-var, no-var, one-var-declaration-per-line, quotes, no-shadow, prefer-arrow-callback, prefer-template, consistent-return, no-return-assign, new-parens, no-param-reassign, max-len */
 
-var Breakpoints = (function() {
-  var BreakpointInstance, instance;
+var instance = null;
+var BREAKPOINTS = ["xs", "sm", "md", "lg"];
 
-  function Breakpoints() {}
+function BreakpointInstance() {
+  this.setup();
+}
 
-  instance = null;
+BreakpointInstance.prototype.setup = function() {
+  var allDeviceSelector, els;
+  allDeviceSelector = BREAKPOINTS.map(function(breakpoint) {
+    return ".device-" + breakpoint;
+  });
+  if ($(allDeviceSelector.join(",")).length) {
+    return;
+  }
+  // Create all the elements
+  els = $.map(BREAKPOINTS, function(breakpoint) {
+    return "<div class='device-" + breakpoint + " visible-" + breakpoint + "'></div>";
+  });
+  return $("body").append(els.join(''));
+};
 
-  BreakpointInstance = (function() {
-    var BREAKPOINTS;
+BreakpointInstance.prototype.visibleDevice = function() {
+  var allDeviceSelector;
+  allDeviceSelector = BREAKPOINTS.map(function(breakpoint) {
+    return ".device-" + breakpoint;
+  });
+  return $(allDeviceSelector.join(",")).filter(":visible");
+};
 
-    BREAKPOINTS = ["xs", "sm", "md", "lg"];
+BreakpointInstance.prototype.getBreakpointSize = function() {
+  var $visibleDevice;
+  $visibleDevice = this.visibleDevice;
+  // TODO: Consider refactoring in light of turbolinks removal.
+  // the page refreshed via turbolinks
+  if (!$visibleDevice().length) {
+    this.setup();
+  }
+  $visibleDevice = this.visibleDevice();
+  return $visibleDevice.attr("class").split("visible-")[1];
+};
 
-    function BreakpointInstance() {
-      this.setup();
-    }
+function Breakpoints() {}
 
-    BreakpointInstance.prototype.setup = function() {
-      var allDeviceSelector, els;
-      allDeviceSelector = BREAKPOINTS.map(function(breakpoint) {
-        return ".device-" + breakpoint;
-      });
-      if ($(allDeviceSelector.join(",")).length) {
-        return;
-      }
-      // Create all the elements
-      els = $.map(BREAKPOINTS, function(breakpoint) {
-        return "<div class='device-" + breakpoint + " visible-" + breakpoint + "'></div>";
-      });
-      return $("body").append(els.join(''));
-    };
-
-    BreakpointInstance.prototype.visibleDevice = function() {
-      var allDeviceSelector;
-      allDeviceSelector = BREAKPOINTS.map(function(breakpoint) {
-        return ".device-" + breakpoint;
-      });
-      return $(allDeviceSelector.join(",")).filter(":visible");
-    };
-
-    BreakpointInstance.prototype.getBreakpointSize = function() {
-      var $visibleDevice;
-      $visibleDevice = this.visibleDevice;
-      // TODO: Consider refactoring in light of turbolinks removal.
-      // the page refreshed via turbolinks
-      if (!$visibleDevice().length) {
-        this.setup();
-      }
-      $visibleDevice = this.visibleDevice();
-      return $visibleDevice.attr("class").split("visible-")[1];
-    };
-
-    return BreakpointInstance;
-  })();
-
-  Breakpoints.get = function() {
-    return instance != null ? instance : instance = new BreakpointInstance;
-  };
-
-  return Breakpoints;
-})();
+Breakpoints.get = function() {
+  return instance != null ? instance : instance = new BreakpointInstance;
+};
 
 $(() => { window.bp = Breakpoints.get(); });
 
