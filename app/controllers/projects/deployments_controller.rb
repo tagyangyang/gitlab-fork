dc25aeae0bf816d7ece7d20c19bf6cf8685501d2
Pawel Chojnacki <pawel@chojnacki.ws>
2017-04-04 11:08:55 +0200

    Extend prometheus service to be able to make queries using specific timeframes
         + simple deployment metrics endpoint
    
        Rename variables to timeframe_* and use simple caching with default ranges as nulls
    
        Working per deployment metrics
    
        Fix tests to run with new metrics code
    
        Instead of Nil return empty Hash

---
+++ app/controllers/projects/deployments_controller.rb
@@ -0,0 +1,24 @@
+class Projects::DeploymentsController < Projects::ApplicationController
+  before_action :authorize_read_deployment!
+
+  def metrics
+    @metrics = deployment.metrics(1.hour)
+
+    if @metrics&.any?
+      @metrics[:deployment_time] = deployment.created_at.utc.to_i
+      render json: @metrics, status: :ok
+    else
+      head :no_content
+    end
+  end
+
+  private
+
+  def deployment
+    @deployment ||= environment.deployments.find_by(iid: params[:id])
+  end
+
+  def environment
+    @environment ||= project.environments.find(params[:environment_id])
+  end
+end
